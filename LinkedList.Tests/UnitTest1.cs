31ad0262bf79a5534b4bd2e31c65833b7854f6d8
wekesa931 <wekesa931>
2019-09-29 22:01:23 +0300

    configured tests

---
+++ LinkedList.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace LinkedList.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void First_node_is_head()
+        {
+            // Arrange
+            // var headNode =
+
+            // // Act
+            // var head = LinkedListHelpers.AddToBeginning('a');
+
+            // // Assert
+            // Assert.Equal(45, theNode.Next.Data);
+        }
+    }
+}
